ac8c96e1782fb81e0203aee360cefe6a636fa9fa
AndriiMaksymiv <an.maksymiv@gmail.com>
2026-02-22 13:01:56 +0200

    typography init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between">content</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <h2>content</h2>
+    </main>
+  );
 }
